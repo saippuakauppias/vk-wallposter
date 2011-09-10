31503ba2089e377db18e388eafa2478f40297455
Denis Veselov <progr-m@yandex.ru>
2011-09-10 04:21:11 +0400

    edited descr in config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?
 
 // VKontakte config
-define('VKEMAIL', 'login');				// vk admin email/login
-define('VKPWD', 'password');			// vk admin password
+define('VKEMAIL', 'login');				// vk email/login
+define('VKPWD', 'password');			// vk password
 
 // Other config
 define('SLEEPTIME', 2);					// in seconds; +rand(1, 4) if this var != 0
